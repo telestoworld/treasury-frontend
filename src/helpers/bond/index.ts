34c5360a392513ee27a29c847c07808d4738df0f
Stuart Gardner <stuarttgardner@gmail.com>
2021-11-27 15:24:09 -0300

    ABI and asset work

--- src/helpers/bond/index.ts
+++ src/helpers/bond/index.ts
@@ -3,7 +3,7 @@ import { LPBond, CustomLPBond } from "./lp-bond";
 import { StableBond, CustomBond } from "./stable-bond";
 
 import MimIcon from "../../assets/tokens/MIM.svg";
-import TELOIcon from "../../assets/tokens/TELO.svg";
+//import TELOIcon from "../../assets/tokens/TELO.svg";
 import MimTelestoIcon from "../../assets/tokens/TELESTO-MIM.svg";
 import TELOTelestoIcon from "../../assets/tokens/TELESTO-TELO.svg";
 
@@ -17,7 +17,7 @@ export const mim = new StableBond({
     bondContractABI: StableBondContract,
     reserveContractAbi: StableReserveContract,
     networkAddrs: {
-        [Networks.TELO]: {
+        [Networks.CELO]: {
             bondAddress: "0x694738E0A438d90487b4a549b201142c1a97B556",
             reserveAddress: "0x130966628846BFd36ff31a822705796e8cb8C18D",
         },
@@ -28,11 +28,11 @@ export const wTELO = new CustomBond({
     name: "wTELO",
     displayName: "wTELO",
     bondToken: "TELO",
-    bondIconSvg: TELOIcon,
+    bondIconSvg: "",
     bondContractABI: WTELOBondContract,
     reserveContractAbi: StableReserveContract,
     networkAddrs: {
-        [Networks.TELO]: {
+        [Networks.CELO]: {
             bondAddress: "0xE02B1AA2c4BE73093BE79d763fdFFC0E3cf67318",
             reserveAddress: "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7",
         },
@@ -47,7 +47,7 @@ export const mimTelesto = new LPBond({
     bondContractABI: LpBondContract,
     reserveContractAbi: LpReserveContract,
     networkAddrs: {
-        [Networks.TELO]: {
+        [Networks.CELO]: {
             bondAddress: "0xA184AE1A71EcAD20E822cB965b99c287590c4FFe",
             reserveAddress: "0x113f413371fc4cc4c9d6416cf1de9dfd7bf747df",
         },
@@ -58,12 +58,12 @@ export const mimTelesto = new LPBond({
 export const TELOTelesto = new CustomLPBond({
     name: "TELO_time_lp",
     displayName: "TELESTO-TELO LP",
-    bondToken: "TELO",
+    bondToken: "",
     bondIconSvg: TELOTelestoIcon,
     bondContractABI: LpBondContract,
     reserveContractAbi: LpReserveContract,
     networkAddrs: {
-        [Networks.TELO]: {
+        [Networks.CELO]: {
             bondAddress: "0xc26850686ce755FFb8690EA156E5A6cf03DcBDE1",
             reserveAddress: "0xf64e1c5B6E17031f5504481Ac8145F4c3eab4917",
         },
